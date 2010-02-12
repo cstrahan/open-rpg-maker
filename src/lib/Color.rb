23707710221837d5cd22105a59ee46192b637249
Charles Strahan <charles.c.strahan@gmail.com>
2010-02-12 03:10:20 -0600

    Fixed setters in class Color

--- src/lib/Color.rb
+++ src/lib/Color.rb
@@ -1,18 +1,41 @@
 class Color
-  attr_accessor :red
-  attr_accessor :green
-  attr_accessor :blue
-  attr_accessor :alpha
+  attr_reader :red
+  attr_reader :green
+  attr_reader :blue
+  attr_reader :alpha
+  
+  def red=(num)
+    @red = check num
+  end
+  
+  def green=(num)
+    @green = check num
+  end
+  
+  def blue=(num)
+    @blue = check num
+  end
+  
+  def alpha=(num)
+    @alpha = check num
+  end
   
   def initialize(red, green, blue, alpha=255)
     set red, green, blue, alpha
   end
   
-  # Not sure if alpha should have a default...
+  # Not sure if alpha should have a default here...
   def set(red, green, blue, alpha=255)
-    @red   = red
-    @green = green
-    @blue  = blue
-    @alpha = alpha
+    red   = red
+    green = green
+    blue  = blue
+    alpha = alpha
   end
+  
+private
+
+  def check(num)
+    num < 0 ? 0 : num > 255 ? 255 : num
+  end
+
 end
